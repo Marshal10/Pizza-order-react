994d065a89b1d737118e8477205d4a48f2998b91
Marshal10 <marshalmaleane123@gmail.com>
2024-09-04 19:29:41 +0530

    Fetching orders,order and order item compoinent

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,5 +1,60 @@
+import { useLoaderData } from "react-router-dom";
+import { getOrder } from "../../services/apiRestaurant";
+import {
+  calcMinutesLeft,
+  formatCurrency,
+  formatDate,
+} from "../../utils/helpers";
+import OrderItem from "./OrderItem";
+
 function Order() {
-  return <div>Order info</div>;
+  const order = useLoaderData();
+  const {
+    id,
+    status,
+    priority,
+    priorityPrice,
+    orderPrice,
+    estimatedDelivery,
+    cart,
+  } = order;
+
+  const deliveryIn = calcMinutesLeft(estimatedDelivery);
+  return (
+    <div>
+      <div>
+        <h2>Order #{id} status</h2>
+        <div>
+          {priority && <span>Priority</span>}
+          <span>{status} order</span>
+        </div>
+      </div>
+      <div>
+        <p>
+          {deliveryIn >= 0
+            ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃`
+            : "Order should have arrived"}
+        </p>
+        <p>(Estimated delivery: {formatDate(estimatedDelivery)})</p>
+      </div>
+      <ul>
+        {cart.map((item) => (
+          <OrderItem item={item} />
+        ))}
+      </ul>
+
+      <div>
+        <p>Price pizza: {formatCurrency(orderPrice)}</p>
+        {priority && <p>Price priority: {formatCurrency(priorityPrice)}</p>}
+        <p>To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}</p>
+      </div>
+    </div>
+  );
+}
+
+export async function loader({ params }) {
+  const order = await getOrder(params.orderID);
+  return order;
 }
 
 export default Order;
